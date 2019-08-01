81366445fda4c4cd26a15478b1f58f38c7148596
sockbot <github@sockbot.com>
2019-08-01 21:06:12 +0000

    eslinted

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,16 +6,16 @@ const breed = args[0];
 request("https://api.thecatapi.com/v1/breeds/search?q=" + breed, (error, response, body) => {
   if (error) {
     // console.log("Error:", error);
-    console.log("Request failed. Check the URL?")
+    console.log("Request failed. Check the URL?");
     return error;
   } else {
     console.log("Response:", response.statusCode);
     const jsonBody = JSON.parse(body);
     console.log("Body:", body);
     if (jsonBody[0]) {
-      console.log(jsonBody[0].description)
+      console.log(jsonBody[0].description);
     } else {
-      console.log("Breed not found")
+      console.log("Breed not found");
     }
   }
-})
+});
